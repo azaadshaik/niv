1a29d4743e75d0c42a6b039fccb17d9d92c56b4a
azaad-shaik <azaad@perrysystems.net>
2023-09-25 11:28:26 +0530

    Assessment form creation completed

--- web/modules/niv/src/Controller/NivController.php
+++ web/modules/niv/src/Controller/NivController.php
@@ -3,6 +3,7 @@
 namespace Drupal\niv\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Entity\webform;
 
 /**
  * Returns responses for niv routes.
@@ -21,5 +22,46 @@ class NivController extends ControllerBase {
 
     return $build;
   }
+
+  public function loadAssessmentForm($profileId,$webformId) {
+
+
+
+    $entity = \Drupal::entityTypeManager()->getStorage('node')->load($profileId);
+    
+$webform = \Drupal::entityTypeManager()->getStorage('webform')->load($webformId);
+
+ $draft = \Drupal::entityTypeManager()
+  ->getStorage('webform_submission')
+  ->loadDraft($webform,$entity);
+  //dump($draft);
+  if(!empty($draft)){
+    $webform = $draft->getWebform();
+
+  }
+  
+  //dump($webform);
+
+  //die;
+
+
+
+// $submissions = \Drupal::entityTypeManager()
+//   ->getStorage('webform_submission')
+//   ->loadFromToken('wi5zi2QrYIlfQVQAGRG0EiQ6kLdw0905VglSxZv05NA',$webform);
+//   $webform = $submissions->getWebform();
+  
+   
+    //$webform = \Drupal\webform\Entity\Webform::load('WEBFORM_ID');
+    $build['content'] = [
+      '#type' => 'webform',
+      '#webform' => $webform,
+      ];
+
+
+    return $build;
+  }
+
+
 
 }
